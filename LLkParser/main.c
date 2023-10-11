d2df5ab5eab6388f587a4daeeecfa3a090af1759
aleksandrstarzhynskyi <a.stargik@gmail.com>
2023-10-11 20:46:56 +0300

    Initial Commit

---
+++ LLkParser/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  LLkParser
+//
+//  Created by Aleksandr Starzhynskyi on 11.10.2023.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
